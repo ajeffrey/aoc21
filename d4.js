32f21eb1af9e1610c12467bcb7a297659712676c
Alex Jeffrey <alex@alexjeffrey.me>
2021-12-04 19:14:09 +0000

    d4 p2

--- d4.js
+++ d4.js
@@ -1,10 +1,26 @@
 const XRANGE = [0, 1, 2, 3, 4];
 const YRANGE = [0, 5, 10, 15, 20];
 
+function getScore(board, pick) {
+  const total = board
+    .filter((n) => n !== "x")
+    .reduce((a, b) => a + parseInt(b, 10), 0);
+
+  return total * parseInt(pick, 10);
+}
+
+function printBoard(board) {
+  const printable = board.map(n => n.length == 1 ? ` ${n}` : n);
+  for(let i = 0; i < 5; i++) {
+    console.log(printable.slice(i * 5, (i * 5) + 5).join(' '));
+  }
+  console.log(board.length + '\n', board);
+}
+
 function playBingo(input) {
   const [picksStr, ...boardStrs] = input.split("\n\n");
 
-  const boards = boardStrs.map((bs) => bs.split(/\s+/g));
+  const boards = boardStrs.map((bs) => bs.split(/\s+/g).filter(n => n));
   const picks = picksStr.split(",");
 
   for (const pick of picks) {
@@ -14,15 +30,40 @@ function playBingo(input) {
       for (let i = 0; i < 5; i++) {
         if (
           XRANGE.every((r) => board[r + i * 5] === "x") ||
-          YRANGE.every((r) => board[r + i * 5] === "x")
+          YRANGE.every((r) => board[r + i] === "x")
         ) {
-          const total = board
-            .filter((n) => n !== "x")
-            .reduce((a, b) => a + parseInt(b, 10), 0);
-          return total * parseInt(pick, 10);
+          return getScore(board, pick);
         }
       }
     }
+  }
+}
+
+function loseBingo(input) {
+  const [picksStr, ...boardStrs] = input.split("\n\n");
+
+  let boards = boardStrs.map((bs) => bs.split(/\s+/g).filter(n => n));
+  const picks = picksStr.split(",");
+
+  for (const pick of picks) {
+    const drop = [];
+    for (let n = 0; n < boards.length; n++) {
+      const board = boards[n].map((n) => (n === pick ? "x" : n));
+      boards[n] = board;
+      for (let i = 0; i < 5; i++) {
+        if (
+          XRANGE.every((r) => board[r + i * 5] === "x") ||
+          YRANGE.every((r) => board[r + i] === "x")
+        ) {
+          drop.push(n);
+          if(drop.length === boards.length) {
+            return getScore(boards[0], pick);
+          }
+        }
+      }
+    }
+
+    boards = boards.filter((b, i) => !drop.includes(i));
   }
 }
 
@@ -629,3 +670,4 @@ const input = `67,31,58,8,79,18,19,45,38,13,40,62,85,10,21,96,56,55,4,36,76,42,3
 60 68 41 86 96`;
 
 console.log(playBingo(input));
+console.log(loseBingo(input));
